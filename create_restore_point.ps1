c554b6f4529cff315df61003b4900828ab1624df
mxsvdrgworks <84321862+mxsvdrgworks@users.noreply.github.com>
2023-03-08 09:21:35 +0100

    Create create_restore_point.ps1

---
+++ create_restore_point.ps1
@@ -0,0 +1,5 @@
+Write-Host "System restore point is enabled for the C system drive"
+Enable-ComputerRestore -Drive"C:"
+Write-Host "System restore point creation was started"
+Checkpoint-Computer -Description "$env:computername_$(Get-Date)" -RestorePointType MODIFY_SETTINGS
+Write-Host "System restore point creation completed at $(Get-Date)"
