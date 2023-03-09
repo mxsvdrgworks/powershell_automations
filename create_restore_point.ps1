fc36cd5b6b9ef472be0bbe092c1bcf837a9d7540
mxsvdrgworks <84321862+mxsvdrgworks@users.noreply.github.com>
2023-03-09 13:39:25 +0100

    Update create_restore_point.ps1

--- create_restore_point.ps1
+++ create_restore_point.ps1
@@ -1,5 +1,14 @@
 #This script helps to create a Windows restore point
 Write-Host "System restore point is enabled for the C system drive"
+#User input
+$counter = 0
+while(!$Host.UI.RawUI.KeyAvailable -and ($counter++ -lt 600))
+{
+      [Threading.Thread]::Sleep( 1000 )
+}
+
+
+#Creating restore point
 Enable-ComputerRestore -Drive"C:"
 Write-Host "System restore point creation was started"
 Checkpoint-Computer -Description "$env:computername_$(Get-Date)" -RestorePointType MODIFY_SETTINGS
